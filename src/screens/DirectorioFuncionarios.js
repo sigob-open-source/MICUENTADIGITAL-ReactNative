576ddcc720bd9f81a7d11461e4c97bdac78a87d1
adrian <adrian.alcaraz@sigob.com.mx>
2022-03-28 15:37:26 -0700

    pantalla funcionario

--- src/screens/DirectorioFuncionarios.js
+++ src/screens/DirectorioFuncionarios.js
@@ -1,5 +1,5 @@
 import React, {Component, useState} from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text,TextInput, TouchableOpacity,ScrollView} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
@@ -8,22 +8,51 @@ import Footer from '../components/Footer';
 
 const DirectorioFunc = () =>{
     return(
-        <View style={{flex:1,}}>
-            <View style={{flex:1, alignItems:'center'}}>
+        
+        <View style={{flex:1,height:'100%'}}>
+            <View style={{flex:1, alignItems:'center', }}>
                 <Header style={styles.header} 
                 item="Peticiones" 
                 imgnotif={require("../../assets/imagenes/notificationGet_icon.png")} 
                 img={require("../../assets/imagenes/header_logo.png")}/>
-
+                
                 <Text style={{color:'black',fontSize:20,fontWeight:'700'}}> Funcionario </Text>
 
+                <View style={styles.textInputContainer}>
+                    <TextInput style={styles.textInputStyle} placeholder='Dependencia/Oficina.*'/>
+                </View>
+
+                <View style={styles.textInputContainer}>
+                    <TextInput style={styles.textInputStyle} placeholder='Busqueda de texto.'/>
+                </View>
             </View>
-            
+            <TouchableOpacity>
+                <View style={styles.searchButton}>
+                    <Text style={styles.buttonText}>Buscar Funcionario</Text>
+                </View>
+            </TouchableOpacity>
+            <Footer style={styles.footer}/>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    searchButton:{
+        width:336,
+        height:46,
+        borderRadius:5,
+        backgroundColor:'#79142A',
+        alignSelf:'center',
+        marginBottom:23,
+        justifyContent:'center'
+    },
+    buttonText:{
+        textAlign:'center',
+        color:'white',
+        fontSize:16,
+        fontWeight:'600'
+    },  
     header:{
         flexDirection:'row',
         height:64,
@@ -49,6 +78,22 @@ const styles = StyleSheet.create({
         shadowRadius: 32,
         shadowOpacity: 0.25,
         elevation: 20,
+    },
+    textInputContainer:{
+        marginTop:21,
+        width:336,
+        height:46,
+        alignSelf:'center',
+        justifyContent:'center',
+        borderRadius:10,
+        backgroundColor:'white',
+        borderWidth:2,
+        borderColor:'white',
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height:3},
+        shadowRadius: 7,
+        shadowOpacity: 0.09,
+        elevation: 5,
     },
 })
 
